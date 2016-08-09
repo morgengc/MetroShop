8cdccdabf842708b2458f909870bdb12b29c9a09
morgengc <Chao.Gao@cisdi.com.cn>
2016-08-09 16:54:21 +0800

    手机上字体太小，调整OK

--- app/js/app.js
+++ app/js/app.js
@@ -8,10 +8,10 @@
  * @main shopcatApp
  */
 var shopcatApp = angular.module('shopcatApp', [
-  'ngRoute',
-  'ngAnimate',
-  'shopcatControllers',
-  'baiduMap'
+	'ngRoute',
+	'ngAnimate',
+	'shopcatControllers',
+	'baiduMap'
 ]);
 
 /**
@@ -22,46 +22,51 @@ var shopcatApp = angular.module('shopcatApp', [
  * @return undefined
  */
 shopcatApp.config(['$routeProvider',
-  function($routeProvider) {
-    // 店铺分页显示
-    $routeProvider.when('/page/:pid', {
-      templateUrl: 'partials/shop-list.html',
-      controller: 'ShopListCtrl'
-    });
+function($routeProvider) {
+	// 店铺分页显示
+	$routeProvider.when('/page/:pid', {
+		templateUrl: 'partials/shop-list.html',
+		controller: 'ShopListCtrl'
+	});
 
-    // 按区域分页显示
-    $routeProvider.when('/district/:did/:pid', {
-      templateUrl: 'partials/shop-list.html',
-      controller: 'ShopDistrictCtrl'
-    });
+	// 按区域分页显示
+	$routeProvider.when('/district/:did/:pid', {
+		templateUrl: 'partials/shop-list.html',
+		controller: 'ShopDistrictCtrl'
+	});
 
-    // 按关键字分页显示
-    $routeProvider.when('/search/:keyword/:pid', {
-      templateUrl: 'partials/shop-list.html',
-      controller: 'ShopSearchCtrl'
-    });
+	// 按关键字分页显示
+	$routeProvider.when('/search/:keyword/:pid', {
+		templateUrl: 'partials/shop-list.html',
+		controller: 'ShopSearchCtrl'
+	});
 
-    // 店铺地图
-    $routeProvider.when('/:Longitude,:Latitude', {
-      templateUrl: 'partials/shop-map.html',
-      controller: 'ShopMapCtrl'
-    });
+	// 店铺地图
+	$routeProvider.when('/:Longitude,:Latitude', {
+		templateUrl: 'partials/shop-map.html',
+		controller: 'ShopMapCtrl'
+	});
 
-    // 关于
-    $routeProvider.when('/about', {
-      templateUrl: 'partials/about.html',
-      controller: 'AboutCtrl'
-    });
+	// 关于
+	$routeProvider.when('/about', {
+		templateUrl: 'partials/about.html',
+		controller: 'AboutCtrl'
+	});
 
-    // 联系
-    $routeProvider.when('/contact', {
-      templateUrl: 'partials/contact.html',
-      controller: 'ContactCtrl'
-    });
+	// 联系
+	$routeProvider.when('/contact', {
+		templateUrl: 'partials/contact.html',
+		controller: 'ContactCtrl'
+	});
 
-    // 默认
-    $routeProvider.otherwise({
-      redirectTo: '/page/1'
-    });
-  }]);
+	// 默认
+	$routeProvider.otherwise({
+		redirectTo: '/page/1'
+	});
+}]);
 
+shopcatApp.controller('NavCtrl', function($scope) {
+	$scope.$on('$locationChangeStart', function(e, next, current) {
+		$scope.page = next.split('#').splice(-1).toString().split('/').splice(1);
+	});
+});
